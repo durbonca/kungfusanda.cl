a35c5c93a31fa6fcd174c891457660d80cc8b596
Manuel Duran <durbonca@gmail.com>
2023-07-02 19:39:17 -0400

    Update index.jsx

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState } from 'react'
-
 import Link from 'next/link'
+import { BsWhatsapp, BsInstagram, BsFacebook } from 'react-icons/bs';
 
 const NavBar = () => {
 const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false)
@@ -9,45 +9,45 @@ const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false)
   return (
     <nav className="bg-gray-800 w-full">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* <!-- Mobile menu button--> */}
-                <button 
-                    type="button" 
-                    className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" 
-                    aria-controls="mobile-menu" 
-                    aria-expanded="false"
-                    onClick={() => setMobileMenuIsOpen(!mobileMenuIsOpen)}
-                >
-                <span className="sr-only">Open main menu</span>
-                {/* <!--
-                    Icon when menu is closed.
-                    Menu open: "hidden", Menu closed: "block"
-                --> */}
-                {mobileMenuIsOpen ? 
-                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-                :
-                <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                </svg> 
-                }
-                {/* <!--
-                    Icon when menu is open.
+            <div className="relative flex h-16 items-center justify-between min-h-fit">
+                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                    {/* <!-- Mobile menu button--> */}
+                    <button 
+                        type="button" 
+                        className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" 
+                        aria-controls="mobile-menu" 
+                        aria-expanded="false"
+                        onClick={() => setMobileMenuIsOpen(!mobileMenuIsOpen)}
+                    >
+                    <span className="sr-only">Open main menu</span>
+                    {/* <!--
+                        Icon when menu is closed.
+                        Menu open: "hidden", Menu closed: "block"
+                    --> */}
+                    {mobileMenuIsOpen ? 
+                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    :
+                    <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg> 
+                    }
+                    {/* <!--
+                        Icon when menu is open.
 
-                    Menu open: "block", Menu closed: "hidden"
-                --> */}
-               
-                </button>
-            </div>
+                        Menu open: "block", Menu closed: "hidden"
+                    --> */}
+                
+                    </button>
+                </div>
                 <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                     {/* <div className="flex flex-shrink-0 items-center">
                         <img className="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
                         <img className="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
                     </div> */}
                     <div className="hidden sm:ml-6 sm:block">
-                    <div className="flex space-x-4">
+                    <div className="flex space-x-4 items-center">
                         {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
                         <Link href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</Link>
                         <Link href="horarios" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Horarios</Link>
@@ -57,6 +57,18 @@ const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false)
                     </div>
                     </div>
                 </div>
+                <div className='justify-end flex items-center gap-4'>
+                    <a href="https://wa.me/56951787063" target="_blank" rel="noreferrer">
+                        <BsWhatsapp className='text-lg lg:text-4xl' />
+                    </a>
+                    <a href="https://www.instagram.com/kungfuguangong/" target="_blank" rel="noreferrer">
+                        <BsInstagram className='text-lg lg:text-4xl'/>
+                    </a>
+                    <a href="https://www.facebook.com/kungfulaflorida/" target="_blank" rel="noreferrer">
+                        <BsFacebook className='text-lg lg:text-4xl'/>
+                    </a>
+                </div>
+
             </div>
         </div>
 
