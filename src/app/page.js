8eea09125b45d93318268fafbaa0efd65c3f585f
Manuel Duran <durbonca@gmail.com>
2023-07-02 19:27:44 -0400

    insert tu tubo

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import VideoCover from '../components/VideoCover'
 import Intro from '../components/Intro'
+import { Youtube } from '@/components/Youtube'
 
 export default function Home() {
   return (
     <div>
       <VideoCover />
       <Intro />
+      <Youtube />
     </div>
   )
 }
--- src/components/Youtube/index.jsx
+++ src/components/Youtube/index.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+'use client';
+import YouTube from 'react-youtube';
 
-const Youtube = () => {
+export const Youtube = () => {
+  const opts = {
+    width: '100%',
+    height: '600',
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+
   return (
-    <div>Youtube</div>
-  )
-}
-
-export default Youtube
+    <YouTube
+      videoId={'amewj79Vzgk'} // defaults -> ''
+      id={'amewj79Vzgk'} // defaults -> ''
+      opts={opts} // defaults -> {}
+      containerClassName="YouTube_Container" // defaults -> ''
+      /* className={} // defaults -> ''
+      title={} // defaults -> ''
+      loading={} // defaults -> undefined
+      onReady={} // defaults -> noop
+      onPlay={} // defaults -> noop
+      onPause={} // defaults -> noop
+      onEnd={} // defaults -> noop
+      onError={} // defaults -> noop
+      onStateChange={} // defaults -> noop
+      onPlaybackRateChange={} // defaults -> noop
+      onPlaybackQualityChange={} // defaults -> noop */
+    />
+  );
+};
