cffd0795f1035cd4e6633c9699b81a3e700a90f5
Manuel Duran <133819510+Mduran-gonoodle@users.noreply.github.com>
2023-07-05 20:40:10 -0400

    Update index.jsx

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -77,6 +77,7 @@ export default function NavBar() {
                             <Menu.Item>
                                 {({ active }) => (
                                 <Link
+                                    key={item.name}
                                     href={item.href}
                                     className={classNames(active ? 'bg-gray-400' : '', 'block px-4 py-2 text-sm text-gray-700')}
                                 >
