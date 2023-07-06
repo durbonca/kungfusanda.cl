8977b9b1cae84c48947b1dec72b171df3a7786a7
Manuel Duran <133819510+Mduran-gonoodle@users.noreply.github.com>
2023-07-05 20:41:51 -0400

    Update index.jsx

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -74,10 +74,9 @@ export default function NavBar() {
                     >
                         <Menu.Items className="absolute left-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-gray-700 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                             { navigationDropdown.map((item) => (
-                            <Menu.Item>
+                            <Menu.Item key={item.name}>
                                 {({ active }) => (
                                 <Link
-                                    key={item.name}
                                     href={item.href}
                                     className={classNames(active ? 'bg-gray-400' : '', 'block px-4 py-2 text-sm text-gray-700')}
                                 >
